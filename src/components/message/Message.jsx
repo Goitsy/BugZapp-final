2fdff2074f36395c1ab743764e210d647d14be93
Goitsy <goitseone.montsho@dci-student.org>
2024-10-05 09:35:55 +0200

    update

---
+++ src/components/message/Message.jsx
@@ -0,0 +1,10 @@
+import "./message.css";
+
+//FaKEAPI___________________________
+import MessageData from "../../FakeAPI/MessageData";
+
+const Message = () => {
+  return <div className="">Message</div>;
+};
+
+export default Message;
