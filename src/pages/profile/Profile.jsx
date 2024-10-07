74333e50af7a617b6de9568007bc7f12e438da90
Goitsy <goitseone.montsho@dci-student.org>
2024-10-07 20:07:49 +0200

    update profile layout

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+//COmponents________________
+
+import AddPost from "../../components/addPost/AddPost";
+import Post from "../../components/addPost/Post";
+import UserProfile from "../../components/userProfile/UserProfile";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <>
+      <UserProfile />
+      <AddPost />
+    </>
+  );
 };
 
 export default Profile;
