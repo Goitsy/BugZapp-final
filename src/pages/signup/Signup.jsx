292fd179a05664e1b275b7f4937f7164c92c2c88
Abdelrahman Ibrahim <abdelrahman.ibrahim@dci-student.org>
2024-10-07 03:04:46 +0200

    signUp Page

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./signup.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import BugZappLogo from "../../assets/favicon/BugZappLogo.png";
 
 export const Signup = () => {
   const handleGoogleLogin = () => {
@@ -11,30 +14,40 @@ export const Signup = () => {
 
   return (
     <div className="signup">
-      <div className="card">
+      <div class="hlogo">
+        Bug<span class="highlight">Zapp</span>
+      </div>
+
+      <div className="card shiny-effect">
         <div className="left">
-          <h2>BugZapp</h2>
+          <div className="logo-img">
+            <img src={BugZappLogo} alt="BugZapp Logo" />
+          </div>
           <p>
-            Welcome to BugZapp – the go-to social platform for programmers to
-            connect, collaborate, and debug together. Join the community and
-            level up your skills today!
+            Welcome to BugZapp – social platform for programmers to connect,
+            collaborate, and debug together. Join the community and level up
+            your skills today!
           </p>
           <span>Have An Account?</span>
           <Link to="/login">
             <button className="btn btn-primary">Login</button>
           </Link>
           <button className="btn-red" onClick={handleGoogleLogin}>
-            Login with Google
+            Login with <FontAwesomeIcon icon={faGoogle} />
           </button>
         </div>
         <form className="right">
+          <h2>Sign Up</h2>
+          <br />
           <input type="text" required placeholder="username" />
           <input type="email" required placeholder="email" />
-          <button type="submit" className="btn">
+          <br />
+          <button type="submit" className="btn btn-login">
             Register
           </button>
         </form>
       </div>
+      <div class="footer">© 2024 BugZapp All rights reserved.</div>
     </div>
   );
 };
